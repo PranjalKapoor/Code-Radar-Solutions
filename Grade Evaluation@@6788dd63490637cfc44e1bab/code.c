82741cf47d82c7123b53f2a612abc7a6ee1616ac
PranjalKapoor <pranjalkapoor9y@gmail.com>
2025-02-11 22:06:26 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
     char alphabet;
-    scanf("%c",&char);
+    scanf("%c",&alphabet);
     switch(alphabet) {
         case 'A': printf("Excellent \n");
                 break;
