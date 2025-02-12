109c39980685202b7ebc79c3dca0789d2b594a5f
PranjalKapoor <pranjalkapoor9y@gmail.com>
2025-02-12 12:29:32 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -6,7 +6,6 @@ int main(){
     scanf("%d%d",&num1,&num2);
     scanf("%c",&character);
     printf("%d",(num1+num2));
-    }
     else if(character == '-'){
         printf("%d",(num1-num2));
     }
