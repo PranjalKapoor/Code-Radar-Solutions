e1cdaee8be1c3486b8e97c7aa05d966ae22402ec
PranjalKapoor <pranjalkapoor9y@gmail.com>
2025-02-12 14:52:48 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -17,7 +17,7 @@ int main(){
             break;
     case '/': 
             if(num2==0){
-                 printf("%d",(num1/num2));}
+                 printf("error");}
             else{
                 printf("%d",(num1/num2));
             }
